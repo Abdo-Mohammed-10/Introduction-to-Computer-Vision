f62367b328b8932e4b194715b1548e81f314ba82
Abdulrahman Mohammed <125462586+Abdo-Mohammed-10@users.noreply.github.com>
2024-05-13 03:23:29 +0300

    Delete SURF.m

--- SURF.m
+++
@@ -1,9 +0,0 @@
-build=imread('th (5).jpeg');
-I= rgb2gray(build);
-imshow(I);
-corners=detectSURFFeatures(I,'NumOctaves',1);
-strongestCorners=selectStrongest(corners,15);
-imshow(I);
-hold on
-plot(strongestCorners)
-hold off
